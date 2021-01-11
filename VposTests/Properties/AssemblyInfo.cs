8739119f74cfda2d224afdbb2ccc3258227a3ab2
Ilton ingui <iltoningui@outlook.com>
2021-01-11 03:36:45 +0100

    re adds the projects

---
+++ VposTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VposTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VposTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b6dd1e41-58fb-46a6-a598-65f75a324c62")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VposTests/VposTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace VposApi.Tests
+{
+    [TestClass]
+    public class VposTests
+    {
+        private Vpos merchant;
+
+        [TestInitialize]
+        public void Init()
+        {
+            merchant = new Vpos();
+        }
+
+        [TestMethod]
+        public void TestShouldCreateANewPaymentRequestTransaction()
+        {
+            Assert.ThrowsException<NotImplementedException>(() => merchant.NewPayment("992563019", "123.45"));
+        }
+
+        [TestMethod]
+        public void TestShouldNotCreateANewPaymentRequestTransactionIfCustomerFormatIsInvalid()
+        {
+            Assert.ThrowsException<NotImplementedException>(() => merchant.NewPayment("99256301", "123.45"));
+        }
+    
+        [TestMethod]
+        public void TestShouldNotCreateANewPaymentRequestTransactionIfAmountFormatIsInvalid()
+        {
+            Assert.ThrowsException<NotImplementedException>(() => merchant.NewPayment("992563019", "123.45.01"));
+        }
+    
+        [TestMethod]
+        public void TestShouldCreateANewRefundRequestTransaction()
+        {
+            Assert.ThrowsException<NotImplementedException>(() => merchant.NewRefund("1jYQryG3Qo4nzaOKgJxzWDs25Hv"));
+        }
+    
+        [TestMethod]
+        public void TestShouldNotCreateANewRefundRequestTransactionIfParentTransactionIdIsNotPresent()
+        {
+            Assert.ThrowsException<NotImplementedException>(() => merchant.NewRefund(null));
+        }
+    
+        [TestMethod]
+        public void TestShouldNotCreateANewRefundRequestTransactionIfSupervisorCardIsInvalid()
+        {
+            Assert.ThrowsException<NotImplementedException>(() => merchant.NewRefund("1jYQryG3Qo4nzaOKgJxzWDs25Hv", supervisorCard:  "123123123123123"));
+        }
+    
+        [TestMethod]
+        public void TestShouldGetAllTransactions()
+        {
+            Assert.ThrowsException<NotImplementedException>(() => merchant.GetTransactions());
+        }
+    
+        [TestMethod]
+        public void TestShouldGetASingleTransaction()
+        {
+            Assert.ThrowsException<NotImplementedException>(() => merchant.GetTransaction("1jYQryG3Qo4nzaOKgJxzWDs25Ht"));
+        }
+         
+        [TestMethod]
+        public void TestShouldNotGetANonExistentSingleTransaction()
+        {
+            Assert.ThrowsException<NotImplementedException>(() => merchant.GetTransaction("1jYQryG3Qo4nzaOKgJxzWDs25H"));
+        }
+    
+        [TestMethod]
+        public void TestShouldGetARunningSingleRequestStatus()
+        {
+            Assert.ThrowsException<NotImplementedException>(() => merchant.NewPayment("925888553", "123.45"));
+        }
+    }
+}
